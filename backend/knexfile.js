1df7eedd67f97196b182502f764cee4f30fbf2d2
Chris Dowd <chrisdowd1996@gmail.com>
2022-05-03 21:01:01 -0500

    backend

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -3,21 +3,13 @@ const path = require("path");
 require("dotenv").config();
 
 // Enviornment Variable handling
-const {
-  NODE_ENV = "development",
-  DEVELOPMENT_DATABASE_URL,
-  PRODUCTION_DATABASE_URL,
-} = process.env;
-const URL =
-  NODE_ENV === "production"
-    ? PRODUCTION_DATABASE_URL
-    : DEVELOPMENT_DATABASE_URL;
+const { DEVELOPMENT_DATABASE_URL } = process.env;
 
 // Config and export
 module.exports = {
   development: {
     client: "postgresql",
-    connection: process.env.DEVELOPMENT_DATABASE_URL,
+    connection: DEVELOPMENT_DATABASE_URL,
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations")
@@ -28,7 +20,7 @@ module.exports = {
   },
   production: {
     client: "postgresql",
-    connection: process.env.PRODUCTION_DATABASE_URL,
+    connection: DEVELOPMENT_DATABASE_URL,
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations")
