732b09c8ad38fc403d58962be9465a75013c6731
Chris Dowd <chrisdowd1996@gmail.com>
2022-05-03 21:07:41 -0500

    localhost changes

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "https://welovemovies-backend-chris.herokuapp.com";
+  process.env.REACT_APP_API_BASE_URL || "https://localhost:5000";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
