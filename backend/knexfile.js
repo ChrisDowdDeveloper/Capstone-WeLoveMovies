d05a3b712674a8ed58532ef82c3e394148508480
Chris Dowd <chrisdowd1996@gmail.com>
2022-05-03 21:11:18 -0500

    trying

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -20,7 +20,7 @@ module.exports = {
   },
   production: {
     client: "postgresql",
-    connection: DEVELOPMENT_DATABASE_URL,
+    connection: process.env.PRODUCTION_DATABASE_URL,
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations")
