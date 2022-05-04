79b706bce54e363fa9f7fe6cf9c63af9c35f6465
Chris Dowd <chrisdowd1996@gmail.com>
2022-05-03 20:18:03 -0500

    db

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -3,7 +3,15 @@ const path = require("path");
 require("dotenv").config();
 
 // Enviornment Variable handling
-const { DEVELOPMENT_DATABASE_URL } = process.env;
+const {
+  NODE_ENV = "development",
+  DEVELOPMENT_DATABASE_URL,
+  PRODUCTION_DATABASE_URL,
+} = process.env;
+const URL =
+  NODE_ENV === "production"
+    ? PRODUCTION_DATABASE_URL
+    : DEVELOPMENT_DATABASE_URL;
 
 // Config and export
 module.exports = {
