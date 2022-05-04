23190fd9f955db342557c37f634a64af52e273af
Chris Dowd <chrisdowd1996@gmail.com>
2022-05-03 20:53:00 -0500

    backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,21 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-
+const knex = require("./db/connection")
+const notFound = require("./errors/notFound");
+const errorHandler = require("./errors/errorHandler");
 const moviesRouter = require("./movies/movies.router");
 const theatersRouter = require("./theaters/theaters.router");
 const reviewsRouter = require("./reviews/reviews.router");
-const notFound = require("./errors/notFound");
-const errorHandler = require("./errors/errorHandler");
-
 const app = express();
+
+/*let corsOptions = {
+    origin: 'https://chris-welovemovies-backend.herokuapp.com',
+    optionsSuccessStatus: 200, // For legacy browser support
+    methods: "GET, PUT, DELETE"
+}*/
+//Trying to fix cors issue, got it at this url "https://stackabuse.com/handling-cors-with-node-js/"
+//must also use: app.use(cors(corsOptions))
 
 app.set("db", knex);
 app.use(cors());
