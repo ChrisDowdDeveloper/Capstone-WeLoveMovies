902000cd1b8640255baad45a23a73497adfc3e09
Chris Dowd <chrisdowd1996@gmail.com>
2023-01-11 19:23:05 -0600

    backend change

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "https://welovemovies-backend-chris.herokuapp.com";
+  process.env.REACT_APP_API_BASE_URL || "https://welovemovies-backend-ldel.onrender.com";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
